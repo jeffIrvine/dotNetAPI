9ea72380da3b5da13ec0936e36498362f906066e
Jeff Irvine (he/him) <68131101+jeffIrvine@users.noreply.github.com>
2023-06-28 21:24:51 -0700

    Items and Users in Mongo!

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -4,8 +4,8 @@ namespace dotNetAPI.Models
     public class UserModel
     {
         public Guid Id { get; set; }
-        public string TrailName { get; set; }
-        public string Email { get; set; }
+        public string ?TrailName { get; set; }
+        public string ?Email { get; set; }
         public List<ItemModel> Items { get; set; } = new();
     }
 }
